849be74e610463d06ea5d8e47caa4036c7feaba1
Balagopal Ambalakkat <bambalakkat@gmail.com>
2014-10-10 22:51:19 -0700

    comment test

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         
+        // 
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
